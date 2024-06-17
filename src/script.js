e537e187c78230260446cac6a75e12027eda32b8
vikas <vikasjamkar2001@gmail.com>
2024-06-17 16:26:28 +0530

    first commit

---
+++ src/script.js
@@ -0,0 +1,26 @@
+let typed = new Typed(".auto-input", {
+  strings: ["Front-End Developer !"],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 2000,
+  loop: true,
+});
+
+AOS.init();
+
+// var icon = document.getElementById("icon");
+// var body = document.querySelector("body");
+// var a = document.querySelector("a");
+
+// icon.addEventListener("click", function () {
+//   this.classList.toggle("bi-moon");
+//   if (this.classList.toggle("bi-brightness-high-fill")) {
+//     body.style.background = "white";
+//     body.style.transition = "2s";
+//     body.style.color = "black";
+//   } else {
+//     body.style.background = "black";
+//     body.style.color = "white";
+//     body.style.transition = "2s";
+//   }
+// });
