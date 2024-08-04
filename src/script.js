dd0e1e2fab2bfdbd1b30e90938b190389b39fe68
vikas <vikasjamkar2001@gmail.com>
2024-08-04 20:10:41 +0530

    navbar commit

--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector("#navArrow");
   const header = document.querySelector("header");
+  const Navbar = document.querySelector("#deskNav");
+  const icon = document.querySelector("#jus");
+  const box = document.querySelector("#container");
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
       nav.style.display = "block";
-      header.style.borderBottom = "1px solid grey";
+      header.style.background = "black";
     } else {
       nav.style.display = "none";
       header.style.background = "";
-      header.style.borderBottom = "";
     }
+  });
+  Navbar.addEventListener("click", () => {
+    icon.classList.toggle("bi-x-lg");
+    box.classList.toggle("nav_container");
   });
 });
 AOS.init();
